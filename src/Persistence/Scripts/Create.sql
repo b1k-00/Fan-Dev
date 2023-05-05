3f0611185878269029d32b387777ccb020e7dc3a
BCollins <brennencollins@bears.mybrcc.edu>
2023-05-05 01:16:45 -0500

    Test commit #001

---
+++ src/Persistence/Scripts/Create.sql
@@ -0,0 +1,22 @@
+USE [Task_Builds]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 5/4/2023 9:19:52 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](50) NOT NULL,
+	[LastName] [varchar](50) NOT NULL,
+	[PhoneNumber] [int] NULL,
+	[Email] [varchar](100) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
