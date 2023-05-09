d31ad947b9b114427a66166999ebe7002c8aa376
BCollins <brennencollins@bears.mybrcc.edu>
2023-05-09 16:11:46 -0500

    Added new table to DB & updated Create.sql script

--- src/Persistence/Scripts/Create.sql
+++ src/Persistence/Scripts/Create.sql
@@ -45,3 +45,36 @@ GO
 
 ALTER TABLE [dbo].[Users_Tasks] CHECK CONSTRAINT [FK_Users_Tasks_Users_Tasks]
 GO
+
+USE [Task_Builds]
+GO
+
+/****** Object:  Table [dbo].[Tasks_Info]    Script Date: 5/9/2023 4:06:34 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tasks_Info](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NULL,
+	[StartTime] [datetime] NULL,
+	[EndTime] [datetime] NULL,
+	[DayOfWeek] [int] NULL,
+ CONSTRAINT [PK_Tasks_Info] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Tasks_Info]  WITH CHECK ADD  CONSTRAINT [FK_Tasks_Info_Tasks_Info] FOREIGN KEY([Id])
+REFERENCES [dbo].[Tasks_Info] ([Id])
+GO
+
+ALTER TABLE [dbo].[Tasks_Info] CHECK CONSTRAINT [FK_Tasks_Info_Tasks_Info]
+GO
+
+
+
