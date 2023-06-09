2f1219b0cf081ce714ac2ebc098c8dcd61b7fca4
b1k <brennencollins@bears.mybrcc.edu>
2023-06-09 00:23:52 -0500

    Updated with Sports_db

--- src/Persistence/Scripts/Create.sql
+++ src/Persistence/Scripts/Create.sql
@@ -1,86 +1,102 @@
-USE [Task_Builds]
+USE [TT_Stats]
 GO
 
-/****** Object:  Table [dbo].[Users]    Script Date: 5/4/2023 9:19:52 PM ******/
+/****** Object:  Table [dbo].[TT_Games]    Script Date: 6/8/2023 10:15:53 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Users](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[TT_Games](
+	[GameId] [int] IDENTITY(1,1) NOT NULL,
+	[Date] [datetime] NOT NULL,
+	[Location] [varchar](50) NULL,
+ CONSTRAINT [PK_TT_Games] PRIMARY KEY CLUSTERED 
+(
+	[GameId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[TT_Information]    Script Date: 6/8/2023 10:15:23 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TT_Information](
+	[PlayerId] [int] IDENTITY(1,1) NOT NULL,
+	[TeamId] [int] NOT NULL,
 	[FirstName] [varchar](50) NOT NULL,
 	[LastName] [varchar](50) NOT NULL,
-	[PhoneNumber] [int] NULL,
-	[Email] [varchar](100) NOT NULL,
- CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+	[JerseyNumber] [int] NOT NULL,
+	[Origin] [varchar](50) NULL,
+ CONSTRAINT [PK_TT_Information] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
+	[PlayerId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
-USE [Task_Builds]
+ALTER TABLE [dbo].[TT_Information]  WITH CHECK ADD  CONSTRAINT [FK_TT_Information_TT_Teams] FOREIGN KEY([TeamId])
+REFERENCES [dbo].[TT_Teams] ([TeamId])
 GO
 
-/****** Object:  Table [dbo].[Users_Tasks]    Script Date: 5/9/2023 4:15:34 PM ******/
+ALTER TABLE [dbo].[TT_Information] CHECK CONSTRAINT [FK_TT_Information_TT_Teams]
+GO
+
+/****** Object:  Table [dbo].[TT_Teams]    Script Date: 6/8/2023 10:15:45 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Users_Tasks](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[UserId] [int] NULL,
-	[TaskId] [int] NULL,
- CONSTRAINT [PK_Users_Tasks] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[TT_Teams](
+	[TeamId] [int] IDENTITY(1,1) NOT NULL,
+	[TeamName] [varchar](50) NOT NULL,
+	[TeamArena] [varchar](50) NULL,
+	[TeamColor] [varchar](50) NULL,
+ CONSTRAINT [PK_TT_Team] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
+	[TeamId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[Users_Tasks]  WITH CHECK ADD  CONSTRAINT [FK_Users_Tasks_Tasks_Info] FOREIGN KEY([TaskId])
-REFERENCES [dbo].[Tasks_Info] ([Id])
-GO
-
-ALTER TABLE [dbo].[Users_Tasks] CHECK CONSTRAINT [FK_Users_Tasks_Tasks_Info]
-GO
-
-ALTER TABLE [dbo].[Users_Tasks]  WITH CHECK ADD  CONSTRAINT [FK_Users_Tasks_Users_Tasks] FOREIGN KEY([UserId])
-REFERENCES [dbo].[Users] ([Id])
-GO
-
-ALTER TABLE [dbo].[Users_Tasks] CHECK CONSTRAINT [FK_Users_Tasks_Users_Tasks]
-GO
-
-/****** Object:  Table [dbo].[Tasks_Info]    Script Date: 5/9/2023 4:06:34 PM ******/
+/****** Object:  Table [dbo].[TT_Information]    Script Date: 6/8/2023 10:15:23 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Tasks_Info](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [varchar](50) NULL,
-	[StartTime] [datetime] NULL,
-	[EndTime] [datetime] NULL,
-	[DayOfWeek] [int] NULL,
- CONSTRAINT [PK_Tasks_Info] PRIMARY KEY CLUSTERED
+
+CREATE TABLE [dbo].[TT_Information](
+	[PlayerId] [int] IDENTITY(1,1) NOT NULL,
+	[TeamId] [int] NOT NULL,
+	[FirstName] [varchar](50) NOT NULL,
+	[LastName] [varchar](50) NOT NULL,
+	[JerseyNumber] [int] NOT NULL,
+	[Origin] [varchar](50) NULL,
+ CONSTRAINT [PK_TT_Information] PRIMARY KEY CLUSTERED 
 (
-	[Id] ASC
+	[PlayerId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[Tasks_Info]  WITH CHECK ADD  CONSTRAINT [FK_Tasks_Info_Tasks_Info] FOREIGN KEY([Id])
-REFERENCES [dbo].[Tasks_Info] ([Id])
+ALTER TABLE [dbo].[TT_Information]  WITH CHECK ADD  CONSTRAINT [FK_TT_Information_TT_Teams] FOREIGN KEY([TeamId])
+REFERENCES [dbo].[TT_Teams] ([TeamId])
 GO
 
-ALTER TABLE [dbo].[Tasks_Info] CHECK CONSTRAINT [FK_Tasks_Info_Tasks_Info]
+ALTER TABLE [dbo].[TT_Information] CHECK CONSTRAINT [FK_TT_Information_TT_Teams]
 GO
+
+
+
+
 
 
 
