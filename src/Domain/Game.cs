1db6cf1de78becd8cf0a972fe3529dffe83a7077
b1k <brennencollins@bears.mybrcc.edu>
2023-06-09 22:57:50 -0500

    added domain class to match db tables & entity

---
+++ src/Domain/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain;
+public class Games : BaseEntity
+{
+    public int GameId { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string Location { get; set; }
+}
---
+++ src/Domain/Team.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain;
+
+public class Team : BaseEntity
+{
+    public int TeamId { get; set; }
+    public string TeamName { get; set; }
+    public string TeamArena { get; set; }
+
+    public string TeamColor { get; set; }
+}
